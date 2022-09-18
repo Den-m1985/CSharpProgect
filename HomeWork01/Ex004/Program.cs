b06fecd13bfba1c1602dc334f4cc1b9c3fa35e62
den-m1985 <denismartyanov244@gmail.com>
2022-09-18 14:46:29 +0300

    4 задача

--- HomeWork01/Ex004/Program.cs
+++ HomeWork01/Ex004/Program.cs
@@ -3,32 +3,34 @@
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
 
-Console.WriteLine("Enter the number");
-int number = Convert.ToInt16(Console.ReadLine());
+//Console.WriteLine("Enter the number");
+//int number = int.Parse(Console.ReadLine());
 
-//for (int i = 0; (number % 2==0); i++)
+//int[] array = new int[number];
+
+ 
+//for (int i = 0; i < array.Length; i++)
 //{
- //   Console.WriteLine();
+ // if (array[i] % 2 == 0)
+ // Console.Write($"{array[i]} ");
 //}
+List<int> evenNumbers = new List<int>(6);
+//int [] array = new int [6];
+for (int i = 0; i < 6; i++)
+Console.Write(evenNumbers[i]);
 
-//int[] array = new int [number];
-//int [] array = {1,2,3,4,5,6};
-//DisplayArray(array);
-//Console.WriteLine();
 
-
- //int number = 2;
- int i = 2;
-            while (i <= number)
-            {
-                int result = number % 2;
-                if (result != 1)
-                {
-                    Console.Write("_"+ number);
-                    i++;
-                }
-                else
-                {
-                    i++;
-                }
-            }
+// int i = 0;
+  //          while (i < number)
+    //        {
+    //            if (number % 2 == 0)
+    //            {
+    //                number++;
+    //                i++;
+    //            }              
+    //            else
+    //            {
+    //              i++;
+    //            }
+    //        }
+    //        Console.Write("_"+ number);
