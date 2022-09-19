e597667bf4184c7d0ea275205aae050b732e54bc
den-m1985 <denismartyanov244@gmail.com>
2022-09-19 11:02:55 +0200

    Сделал задачку 4

--- HomeWork01/Ex004/Program.cs
+++ HomeWork01/Ex004/Program.cs
@@ -3,34 +3,12 @@
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
 
-//Console.WriteLine("Enter the number");
-//int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt16(Console.ReadLine());
 
-//int[] array = new int[number];
-
- 
-//for (int i = 0; i < array.Length; i++)
-//{
- // if (array[i] % 2 == 0)
- // Console.Write($"{array[i]} ");
-//}
-List<int> evenNumbers = new List<int>(6);
-//int [] array = new int [6];
-for (int i = 0; i < 6; i++)
-Console.Write(evenNumbers[i]);
-
-
-// int i = 0;
-  //          while (i < number)
-    //        {
-    //            if (number % 2 == 0)
-    //            {
-    //                number++;
-    //                i++;
-    //            }              
-    //            else
-    //            {
-    //              i++;
-    //            }
-    //        }
-    //        Console.Write("_"+ number);
+int [] EvenNumber = Enumerable.Range(0, number+1).ToArray();
+for (int i = 2; i < EvenNumber.Length; i++)
+{
+ if (EvenNumber[i] % 2 == 0)
+ Console.Write($"{EvenNumber[i]} ");
+}
