8c01a589a6cee32e12bd1fb5149b745b1f0c8316
den-m1985 <denismartyanov244@gmail.com>
2022-09-20 12:13:35 +0200

    Домашка02 первая

---
+++ HomeWork02/Ex01/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите 3-х значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+/*
+int a = 12545;
+string str = a.ToString();
+Console.WriteLine(str[4]);
+*/
+
+int k= number;
+while (k >= 100) k /= 10;
+{
+    if (number >= 100)
+    {
+        int d = k % 10;
+        Console.Write("Второе число ");
+        Console.WriteLine(d);
+    } 
+    else
+    {
+        Console.WriteLine("Упссс, что-то пошло не так. Не хватает цифр, или поменяй знак на +");
+    }
+    
+}
+Console.Write("Нажми пробел чтоб выйти");
+Console.ReadKey();
