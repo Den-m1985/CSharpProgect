60f94038485cae9086af9cefcbe36746454a885d
den-m1985 <denismartyanov244@gmail.com>
2022-09-23 10:29:38 +0200

    Сделал 2 задачу

--- HomeWork02/Ex02/Program.cs
+++ HomeWork02/Ex02/Program.cs
@@ -5,16 +5,41 @@
 
 Console.Clear();
 Console.WriteLine("Введите число");
-int Numbers = Convert.ToInt32(Console.ReadLine());
-
-if (Numbers >= 100 && Numbers <= 99999)
+int source = Convert.ToInt32(Console.ReadLine());
+int number = 0;
+int thirdNumber = 0;
+if (source > 99 && source <= 99999)
 {
-    Console.WriteLine("Третья цифра: {0} ", Numbers.ToString().ToCharArray()[2]);
+    if (source > 99)
+    {
+        number = source;
+    }
+    if (source > 999)
+    {
+        number = source / 10 % 10;
+    }
+    if (source > 9999)
+    {
+        number = source / 100 % 10;
+    }
+    if (source > 99999)
+    {
+        number = source / 1000 % 10;
+    }
+    else
+    {
+        thirdNumber = number % 10;
+        Console.Write("Третье число ");
+        Console.WriteLine(thirdNumber);
+    }
+
+    //Console.WriteLine("Третья цифра: {0} ", source.ToString().ToCharArray()[2]);
 }
 else
 {
     Console.WriteLine("Упссс, что-то пошло не так. Не хватает цифр, их много, или поменяй знак на +");
 }
+
 
 Console.Write("Нажми пробел чтобы выйти");
 Console.ReadKey();
