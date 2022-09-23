8023b0ff3e812e6ea7b8cda4ae80494b49472dda
den-m1985 <denismartyanov244@gmail.com>
2022-09-23 14:13:23 +0200

    Сделал 4ю задачку

--- HomeWork02/Ex04/Program.cs
+++ HomeWork02/Ex04/Program.cs
@@ -2,3 +2,29 @@
 //1 2 3 4 5 6 7-> 4
 //10 20 30 40 50 60 70 -> 40
 
+//Немного доработал условие задачи чтоб не прописывать массив вручную.
+
+Console.Clear();
+
+int[] array = new int[7];
+int Length = array.Length;
+
+for (int i = 0; i < Length; i++)
+{
+    array[i] = new Random().Next(1, 7);
+}
+var str = String.Join(" ", array);
+int sum = 0;
+int su = 0;
+for (int i = 0; i < Length; i++)
+{
+    sum = sum + array[i];
+    su = sum / Length;
+}
+
+Console.WriteLine(str);
+Console.Write("кол-во цифр в массиве: ");
+Console.WriteLine(Length);
+Console.Write("Среднеарифметическое: ");
+Console.Write(su);
+
