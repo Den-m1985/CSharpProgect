7eda68dac3628a2c34886d7c7085eebeaa2eaa55
den-m1985 <denismartyanov244@gmail.com>
2022-09-26 16:23:56 +0200

    Начал писать 1задачку

--- HomeWork03/Ex01/Program.cs
+++ HomeWork03/Ex01/Program.cs
@@ -5,3 +5,35 @@
 */
 
 
+int ReedData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+
+string PolyandrTest(int num)
+{
+   if (num /10000 == num%10 && num /1000 == (num/10)%10)
+    
+    return "число полиндром";
+    else
+    return "число неполиндром";
+}
+
+
+if (num >= 10000 && num < 100000)
+{
+    PrintData();
+}
+else
+{
+    PrintData();
+}
