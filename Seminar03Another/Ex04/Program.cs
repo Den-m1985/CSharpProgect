01b3628c06c7e60bcd8ce8f66516f7736240a56b
den-m1985 <denismartyanov244@gmail.com>
2022-09-26 15:21:48 +0200

    Записал 4ю задачку

---
+++ Seminar03Another/Ex04/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты 2 точек и
+находит расстояние между ними в 2D пространстве.
+А(3,6); В(2,1)  = 5,09
+А(7,-5); В(1-,1)   =  7,21
+*/
+
+
+int ReedData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+
+double FindLen(int x1, int y1, int x2, int y2)
+{
+    return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+}
+
+
+int x1 = ReedData("Введите координату х первой точки: ");
+int y1 = ReedData("Введите координату у первой точки: ");
+int x2 = ReedData("Введите координату х второй точки: ");
+int y2 = ReedData("Введите координату у второй точки: ");
+
+double result = FindLen(x1,y1,x2,y2);
+result = Math.Round(result, 3);
+
+PrintData("Расстояние между точками: ",result.ToString());
