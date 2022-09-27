2a9a77d983aa95196699d336fa1462f3c1d55b32
den-m1985 <denismartyanov244@gmail.com>
2022-09-27 22:09:28 +0200

    Сделал 3ю задачку домашки

--- HomeWork03/Ex03/Program.cs
+++ HomeWork03/Ex03/Program.cs
@@ -3,3 +3,35 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125 */
 
+int ReedData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+
+string LineNum(int numN, int p)
+{
+    string outLine = "1";
+    for (int i = 2; i <= numN; i++)
+    {
+        outLine = outLine + "\t" + Math.Pow(i, p); // \t нужна для выравнивания верхнего и нижнего рядов.
+    }
+    return outLine;
+}
+
+
+int numN = ReedData("Введите число N: ");
+
+string lineTop = LineNum(numN, 1);
+string lineDown = LineNum(numN, 3);
+
+PrintData("", lineTop);
+PrintData("", lineDown);
