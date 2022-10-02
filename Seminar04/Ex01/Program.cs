6b5da232b53074cee060dc2c40e2ca9b1f1933f5
den-m1985 <denismartyanov244@gmail.com>
2022-10-02 14:39:56 +0200

    Записал семинар 4

---
+++ Seminar04/Ex01/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 35
+//Задайте одномерный массив из 123 случайных чисел
+//Найдите кол-во элементов массива, значения которых
+//лежат в отрезке(10,99)
+
+//Создаем массив
+int[] Create(int len)
+{
+    return new int[len];
+}
+
+// Заполняем массив
+void Fill(int[] array, int min, int max)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+// печатаем массив
+string Print(int[] array)
+{
+    return "[" + String.Join(", ", array) + "]";
+    //Скобки "[" для эфектного вывода.
+}
+
+/*"[" + String.Join(", ", array) +"]";
+Эту строчку можно записать по другому:
+for (int i = 0; i < array.length; i++)
+{
+    if (i<array.length-1) result = result + array[i] + ", ";      
+    else result = result + array[i]
+}      */
+
+
+int Search(int[] array, int findMin, int findMax)
+{
+    int size = array.Length;
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i] >= findMin && array[i] <= findMax)
+    {
+        count++;
+    }
+}
+return count;
+}
+
+
+int[] arr = Create(12);
+Console.WriteLine(arr);
+Fill(arr, 1, 200);
+Console.WriteLine(Print(arr));
+Console.WriteLine(Search(arr, 10 ,99));
+
---
+++ Seminar04/Speed/Program.cs
@@ -0,0 +1,15 @@
+﻿// Эта програмка расчитывает скорость работы кода
+
+
+
+DateTime dtStart = DateTime.Now;
+
+ int [] arr = new int [100_000_000];
+ int r = 0;
+ for (int i = 0; i < arr.Length; i++)
+ {
+    r += arr[i];
+ }
+
+DateTime dtEnd = DateTime.Now;
+Console.WriteLine((dtEnd - dtStart).TotalMilliseconds);
