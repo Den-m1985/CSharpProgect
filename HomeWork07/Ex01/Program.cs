875a5b56f685b26578c7fac0c48a7a57d3ed1263
den-m1985 <denismartyanov244@gmail.com>
2022-10-07 16:58:36 +0200

    Делаю 2ю задачку

---
+++ HomeWork07/Ex01/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+
+double[,] CreateArray (int a, int b)
+{
+    double [,] array = new double [a,b];
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            array[i,j] = new Random().NextDouble()*10-5;
+            //Console.Write("{0,6:F2}",ar[i,j]);
+        }
+    //Console.WriteLine();
+    }
+    return array;
+}
+
+
+// выводим двухмерный массив в консоль с табуляцией по столбцам
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            //Console.Write($"{array[i, j]} \t");
+            Console.Write("{0,6:F1}",array[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+double[,] result = CreateArray(3,4);
+PrintArray(result);
+//Console.Write("{0,6:F2}",Array1(2,3));
---
+++ HomeWork07/Ex02/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+
+int[,] CreateArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+//Находим число
+int FindDigit(int[,] array, int a, int b)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j] == a)result = array[i,j];
+        }
+    }
+    return result;
+}
+
+int[,]array = CreateArray(4, 4);
+PrintArray(array);
+int result = FindDigit(array,1,5);
+Console.WriteLine(result);
