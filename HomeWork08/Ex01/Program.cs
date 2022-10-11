ae7a86d8f59da77de99094a426901feacced1b15
den-m1985 <denismartyanov244@gmail.com>
2022-10-11 15:17:06 +0200

    Сделал 1 и 2 задачу 8 семинара

---
+++ HomeWork08/Ex01/Program.cs
@@ -0,0 +1,97 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив.
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и
+//выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+
+int[,] CreateArray(int size)
+{
+    int[,] array = new int[size, size];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[] Calculate(int[,] array, int size)
+{
+    int[] sum = new int[size];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum[i] += array[i, j];
+        }
+    }
+    return sum;
+}
+
+
+void PrintArraySum(int[] array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+
+        Console.Write(array[i]);
+        Console.WriteLine();
+    }
+}
+
+
+void FindMin(int[] array, int size)
+{
+    int min = array[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] == min) Console.WriteLine($"Строка с наименьшей суммой: {i}");
+    }
+}
+
+
+int size = ReadData("Введите сторону квадрата: ");
+int[,] arr = CreateArray(size);
+PrintArray(arr);
+
+Console.WriteLine("Суммы по строчкам:");
+int[] sum = Calculate(arr, size);
+PrintArraySum(sum, size);
+
+FindMin(sum, size);
---
+++ HomeWork08/Ex02/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 58: Задайте две матрицы.
+//Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+/*
+int[,] a = {{2,4},
+          {3,2}};
+int[,] b = {{3,4},
+           {3,3}};
+           
+// a[0,0]*b[0,0] + a[0,1]*b[1,0]  |  a[0,0]*b[0,1] + a[0,1]*b[1,1]
+
+// a[1,0]*b[0,0] + a[1,1]*b[1,0]  |  a[1,0]*b[0,1] + a[1,1]*b[1,1]
+*/
+
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+
+int[,] CreateArray(int size)
+{
+    int[,] array = new int[size, size];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void Calculate(int[,] array1, int[,] array2, int size)
+{
+    int[,] matrixSum = new int[size, size];
+
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            for (int k = 0; k < array2.GetLength(0); k++)
+            {
+                matrixSum[i, j] = matrixSum[i, j] + array1[i, k] * array2[k, j];
+            }
+            Console.Write(matrixSum[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int length = ReadData("Введите сторону массива: ");
+int[,] arr1 = CreateArray(length);
+int[,] arr2 = CreateArray(length);
+
+Console.WriteLine("Матрица 1:");
+PrintArray(arr1);
+Console.WriteLine("Матрица 2:");
+PrintArray(arr2);
+
+Console.WriteLine("Произведение матриц:");
+Calculate(arr1, arr2, length);
