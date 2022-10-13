adc64ee35af42d2a3d1fe92e5c2af35e6457b287
den-m1985 <denismartyanov244@gmail.com>
2022-10-13 16:56:43 +0200

    Task spiral

---
+++ Different/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+// задачка соспиральным массивом.
+int N = 4;
+int val = 1;
+int x = -1;
+int y = 0;
+int[,] array = new int[N, N];
+for (int i = 1; i < 2 * N; i++)
+{
+    for (int j = 0; j < N - i / 2; j++)
+    {
+        array[y += (i % 4 - 1) % 2, x -= (i % 4 - 2) % 2] = val++;
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
