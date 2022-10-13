3c8eec2024851cedb4332431fb947d93a26f46be
den-m1985 <denismartyanov244@gmail.com>
2022-10-13 12:46:08 +0200

    Записал 8ю лекцию

---
+++ Lesson08/Ex05/Program.cs
@@ -0,0 +1,50 @@
+﻿
+decimal fRec = 0;
+decimal fIte = 0;
+
+decimal FibonacciRecurtion(int n)
+{
+    fRec++;
+    return n == 0 || n == 1 ? 1 : FibonacciRecurtion(n - 1) + FibonacciRecurtion(n - 2);
+}
+
+decimal FibonacciItaration(int n)
+{
+    fRec++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 2; i <=n; i++)
+    {
+        result = f0 + f1;
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+
+
+
+//Засекаем время
+DateTime dt = DateTime.Now;
+
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciItaration({n}) = {FibonacciItaration(n)} fIte = {fIte.ToString("### ### ###"), -15}");
+    fIte = 0;
+}
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+
+Console.WriteLine();
+
+
+//Засекаем время
+dt = DateTime.Now;
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciRecurtion({n}) = {FibonacciRecurtion(n)} fRec = {fRec.ToString("### ### ###"), -15}");
+    fRec = 0;
+}
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
---
+++ Lesson08/Ex06/Program.cs
@@ -0,0 +1,43 @@
+﻿// 
+
+int[,] pic = new int[10, 10];
+for (int i = 0; i < 10; i++)
+{
+    pic[0, i] = 1;
+    pic[i, 0] = 1;
+    pic[i, 9] = 1;
+    pic[9, i] = 1;
+}
+
+PrintImage(pic);
+fillImage(1, 1);
+Console.WriteLine();
+PrintImage(pic);
+fillImage(1, 1);
+
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            if (image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void fillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        fillImage(row - 1, col);
+        fillImage(row, col - 1);
+        fillImage(row + 1, col);
+        fillImage(row, col + 1);
+    }
+}
+
