926838768014a6e0c5439a9ce6b854340aeed6df
den-m1985 <denismartyanov244@gmail.com>
2022-10-20 10:11:11 +0200

    Task 35

--- KamianetskiiTask/Task35/Program.cs
+++ KamianetskiiTask/Task35/Program.cs
@@ -2,3 +2,38 @@
 
 
 
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    Console.Write("[" + String.Join(", ", array) + "]");
+    return array;
+}
+
+
+void Calculate(int[] array, int size, int number)
+{
+    int count =0;
+    for (int i = 0; i < size; i++)
+    {
+        if (number == array[i]) count ++;
+    }
+
+    if (count == 0) Console.Write($"Random number {number} is not present in thise array");
+    else Console.Write($"Random number {number} is present in thise array");
+}
+
+
+int size = 12;
+Console.WriteLine();
+int[] array = CreateArray(size);
+Console.WriteLine();
+int number = new Random().Next(-10, 10);
+Calculate(array, size, number);
+Console.WriteLine();
+
+
+
