a065368daee5ae4edba640011a9e67e08b333562
den-m1985 <denismartyanov244@gmail.com>
2022-10-20 09:22:45 +0200

    Task 32,33

--- KamianetskiiTask/Task32/Program.cs
+++ KamianetskiiTask/Task32/Program.cs
@@ -1,3 +1,14 @@
 ﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
 
+int size = 8;
+
+int [] array = new int [size];
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(0,2);
+    Console.Write(array[i] +"\t");
+}
+
+
+
 
--- KamianetskiiTask/Task33/Program.cs
+++ KamianetskiiTask/Task33/Program.cs
@@ -2,4 +2,44 @@
 //Найти сумму положительных/отрицательных элементов массива
 
 
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    Console.Write("[" + String.Join(", ", array) + "]");
+    return array;
+}
+
+
+void Calculate(int[] array, int size)
+{
+    int positiveNumber = 0;
+    int negativeNumber = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            positiveNumber += array[i];
+        }
+        else
+        {
+            negativeNumber += array[i];
+        }
+    }
+    Console.Write("Sum positiveNumber: ");
+    Console.WriteLine(positiveNumber);
+    Console.Write("Sum negativeNumber: ");
+    Console.WriteLine(negativeNumber);
+
+}
+
+
+int size = 12;
+int[] array = CreateArray(size);
+Console.WriteLine();
+Calculate(array, size);
+
 
