3b237d3929d1e4f9351d562f7196547579c7a692
den-m1985 <denismartyanov244@gmail.com>
2022-10-20 10:20:45 +0200

    Task 37

--- KamianetskiiTask/Task37/Program.cs
+++ KamianetskiiTask/Task37/Program.cs
@@ -2,3 +2,39 @@
 
 
 
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 999);
+    }
+    Console.Write("[" + String.Join(", ", array) + "]");
+    return array;
+}
+
+
+void Calculate(int[] array, int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] >= 10 && array[i] <=99)
+        {
+            count++;
+        }
+        
+    }
+    Console.Write("Count numbers: ");
+    Console.WriteLine(count);
+   
+
+}
+
+
+int size = 123;
+Console.WriteLine();
+int[] array = CreateArray(size);
+Console.WriteLine();
+Calculate(array, size);
+
